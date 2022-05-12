cc8adc1291fe01e915fe7642a39f40963d0a3d17
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-05-12 22:15:31 +0100

    todo app

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+/* eslint-disable array-callback-return */
+/* eslint-disable no-undef */
+import React, { Component } from 'react';
+import './App.css'
+class App extends Component {
+ 
+   constructor() {
+     super(); 
+     this.state ={
+       message: 'Hello world',
+       newTodo: '',
+       todos: [{
+         tittle: 'Learn Raect',
+          done: false
+       }, {
+         tittle: 'learn JXS',
+         done: false
+       }]
+     };
+   }
+   foremSubmited(event) {
+     event.preventDefault();
+     this.setState({
+       newTodo: '',
+      todos: [...this.state.todos, {
+        tittle: this.state.newTodo,
+        done: false
+      }]
+    })
+   }
+
+   newTodoCganged (event) {
+    this.setState({
+      newTodo: event.target.value
+    });
+    
+   }
+   toggleTodoDone(event){
+     console.log(event.target.checked);
+     const todos = [...this.state.todos];
+      todos[index] = {...todos[index] };
+     todos[index].done = event.target.checked;
+     console.log(todos);
+    this.setState({
+       todos
+       
+    })
+   }
+
+   removeTodo(index){
+    const todos = [...this.state.todos];
+    todos.splice(index, 1)
+    this.setState({
+      todos
+      
+   })
+   }
+
+   allDoen(){
+      const todos  = this.state.todos.map(todo => {
+        console.log(todo);
+           return{
+             
+             ...todo,
+             done : true
+           }
+      })
+
+       this.setState({
+       todos
+       
+    })
+   }
+   render() {
+    return (
+      <div className='App'>
+      <div>{this.state.message}</div>
+      <form onSubmit={(event)=> this.foremSubmited(event)}>
+        <label htmlFor='newTodo'> New Todo</label>
+        <input onChange={ (event)=> this.newTodoCganged(event)} id='newTodo' name='newTodo'/>
+        <button type='submit'>Add todo</button>
+      </form>
+      <button onClick={()=> this.allDoen() }>All done </button>
+      <ul>
+         {this.state.todos.map((todo , index) =>{
+          return (<li key={todo.tittle}>
+            <input  onChange={(event) => this.toggleTodoDone(event, index)} type='checkBox' checked={todo.done}/>
+          <span style={{
+       textDecoration: todo.done ? "line-through" : 'none'
+            }}>  {todo.tittle } </span>
+            <button onClick={()  => this.removeTodo(index) }> Remove</button>
+            </li>)
+         })}
+      </ul>
+      </div>
+    )
+  }
 }
+export default App
 
-export default App;
+
